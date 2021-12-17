f827a99b1c6fc114dd802420f80a884be32f2263
nikitapoly <nikitapoly@comcast.net>
2021-12-16 22:31:23 -0500

    finished contatc picking js

--- Public/JS/formPicker.js
+++ Public/JS/formPicker.js
@@ -1,7 +1,10 @@
 "use strict";
-let buttons = document.querySelectorAll("div#bannerDiv ul li button");
+const buttons = document.querySelectorAll("div#bannerDiv ul li button");
+const allFormTemplates = document.getElementsByTagName("template")[0].content;
+const formDiv = document.getElementById("formDiv");
 for (let button of buttons) {
     button.addEventListener("click", () => {
-        console.log(button.innerHTML);
+        formDiv.innerHTML = "";
+        formDiv.appendChild(allFormTemplates.querySelector(`form#${button.innerText.replace(" ", "")}`).cloneNode(true));
     });
 }
--- TS/formPicker.ts
+++ TS/formPicker.ts
@@ -1,6 +1,11 @@
-let buttons: NodeListOf<HTMLButtonElement> = document.querySelectorAll("div#bannerDiv ul li button")
-for(let button of buttons){
-    button.addEventListener("click",()=>{
-        console.log(button.innerHTML);
+const buttons: NodeListOf<HTMLButtonElement> = document.querySelectorAll("div#bannerDiv ul li button")//get all buttons
+
+const allFormTemplates: DocumentFragment = document.getElementsByTagName("template")[0].content//get the forms from the template
+const formDiv: HTMLElement  = document.getElementById("formDiv")
+
+for(let button of buttons){//add event listener to change the form
+    button.addEventListener("click",()=>{//display the form in the main page
+        formDiv.innerHTML = ""
+        formDiv.appendChild(allFormTemplates.querySelector(`form#${button.innerText.replace(" ","")}`).cloneNode(true))
     })
 }
